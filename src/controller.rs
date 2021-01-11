b7e391a4f3c124fa7fb8bc14e23937c502ac65f9
Margatroid <i@margatroid.xyz>
2021-01-11 09:38:09 +0800

    refactor: split code and add log

---
+++ src/controller.rs
@@ -0,0 +1,135 @@
+use std::sync::Arc;
+
+use bytes::BufMut;
+use chrono::prelude::*;
+use futures::TryStreamExt;
+use log::info;
+use warp::{http, multipart::Part};
+use warp::{multipart::FormData, Buf};
+use warp::{Rejection, Reply};
+enum UploadStatus {
+    Created,
+    Existed,
+}
+
+impl ToString for UploadStatus {
+    fn to_string(&self) -> String {
+        match self {
+            UploadStatus::Created => String::from("created"),
+            UploadStatus::Existed => String::from("existed"),
+        }
+    }
+}
+
+struct UploadResponse {
+    date: String,
+    digest: String,
+    short: String,
+    size: usize,
+    status: UploadStatus,
+    url: String,
+}
+
+impl ToString for UploadResponse {
+    fn to_string(&self) -> String {
+        return format!(
+            "date: {}
+digest: {}
+short: {}
+size: {}
+url: {}
+status: {}
+",
+            self.date,
+            self.digest,
+            self.short,
+            self.size,
+            self.url,
+            self.status.to_string()
+        );
+    }
+}
+
+pub async fn upload(
+    form: FormData,
+    db: Arc<sled::Db>,
+    url: String,
+) -> Result<impl Reply, Rejection> {
+    let parts: Vec<Part> = form.try_collect().await.map_err(|e| {
+        eprintln!("form error: {}", e);
+        warp::reject::reject()
+    })?;
+    for p in parts {
+        if p.name() == "c" || p.name() == "content" {
+            let value = p
+                .stream()
+                .try_fold(Vec::new(), |mut vec, data| {
+                    vec.put(data.bytes());
+                    async move { Ok(vec) }
+                })
+                .await
+                .map_err(|e| {
+                    log::error!("reading file error: {}", e);
+                    warp::reject::reject()
+                })?;
+            let hash = blake3::hash(&value);
+            // let hash_bytes = hash.as_bytes();
+            let short = &hash.to_hex()[0..4];
+            let date: DateTime<Local> = Local::now();
+            let existed = db.get(short).unwrap().is_some();
+            if !existed {
+                db.insert(short, value.clone()).unwrap();
+            }
+
+            let response = UploadResponse {
+                date: date.to_string(),
+                digest: hash.to_hex().to_string(),
+                short: String::from(short),
+                size: value.len(),
+                status: if existed {
+                    UploadStatus::Existed
+                } else {
+                    UploadStatus::Created
+                },
+                url: format!("{}/{}", url, short),
+            };
+            info!(
+                "{} {} of length {}",
+                response.status.to_string(),
+                response.short,
+                response.size
+            );
+            if existed {
+                return Ok(warp::reply::with_status(
+                    response.to_string(),
+                    http::StatusCode::FOUND,
+                ));
+            } else {
+                return Ok(warp::reply::with_status(
+                    response.to_string(),
+                    http::StatusCode::CREATED,
+                ));
+            }
+        }
+    }
+    Ok(warp::reply::with_status(
+        String::from("invalid"),
+        http::StatusCode::BAD_REQUEST,
+    ))
+}
+
+pub async fn view_data(key: String, db: Arc<sled::Db>) -> Result<impl Reply, Rejection> {
+    if let Ok(Some(data)) = db.get(key.to_lowercase().as_str()) {
+        info!("get {} success", key);
+        return Ok(warp::reply::with_status(
+            String::from_utf8_lossy(&data).to_string(),
+            http::StatusCode::FOUND,
+        ));
+    } else {
+        info!("get {} failed", key);
+        return Ok(warp::reply::with_status(
+            String::from("not found"),
+            http::StatusCode::NOT_FOUND,
+        ));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,12 @@
-use std::{format, sync::Arc};
-
-use bytes::BufMut;
-use chrono::prelude::*;
+use std::{sync::Arc};
 use config::Config;
-use futures::TryStreamExt;
-use log::info;
-use warp::{http, multipart::Part};
-use warp::{multipart::FormData, Buf};
-use warp::{Filter, Rejection, Reply};
+
+use warp::{Filter};
 mod config;
-
-enum UploadStatus {
-    Created,
-    Existed,
-}
-
-impl ToString for UploadStatus {
-    fn to_string(&self) -> String {
-        match self {
-            UploadStatus::Created => String::from("created"),
-            UploadStatus::Existed => String::from("existed"),
-        }
-    }
-}
-
-struct UploadResponse {
-    date: String,
-    digest: String,
-    short: String,
-    size: usize,
-    status: UploadStatus,
-    url: String,
-}
-
-impl ToString for UploadResponse {
-    fn to_string(&self) -> String {
-        return format!(
-            "date: {}
-digest: {}
-short: {}
-size: {}
-url: {}
-status: {}
-",
-            self.date,
-            self.digest,
-            self.short,
-            self.size,
-            self.url,
-            self.status.to_string()
-        );
-    }
-}
-
-async fn upload(form: FormData, db: Arc<sled::Db>, url: String) -> Result<impl Reply, Rejection> {
-    let parts: Vec<Part> = form.try_collect().await.map_err(|e| {
-        eprintln!("form error: {}", e);
-        warp::reject::reject()
-    })?;
-    for p in parts {
-        if p.name() == "c" || p.name() == "content" {
-            let value = p
-                .stream()
-                .try_fold(Vec::new(), |mut vec, data| {
-                    vec.put(data.bytes());
-                    async move { Ok(vec) }
-                })
-                .await
-                .map_err(|e| {
-                    eprintln!("reading file error: {}", e);
-                    warp::reject::reject()
-                })?;
-            let hash = blake3::hash(&value);
-            // let hash_bytes = hash.as_bytes();
-            let short = &hash.to_hex()[0..4];
-            let date: DateTime<Local> = Local::now();
-            let existed = db.get(short).unwrap().is_some();
-            if !existed {
-                db.insert(short, value.clone()).unwrap();
-            }
-
-            let response = UploadResponse {
-                date: date.to_string(),
-                digest: hash.to_hex().to_string(),
-                short: String::from(short),
-                size: value.len(),
-                status: if existed {
-                    UploadStatus::Existed
-                } else {
-                    UploadStatus::Created
-                },
-                url: format!("{}/{}", url, short),
-            };
-            info!(
-                "{} {} of length {}",
-                response.status.to_string(),
-                response.short,
-                response.size
-            );
-            if existed {
-                return Ok(warp::reply::with_status(
-                    response.to_string(),
-                    http::StatusCode::FOUND,
-                ));
-            } else {
-                return Ok(warp::reply::with_status(
-                    response.to_string(),
-                    http::StatusCode::CREATED,
-                ));
-            }
-        }
-    }
-    Ok(warp::reply::with_status(
-        String::from("invalid"),
-        http::StatusCode::BAD_REQUEST,
-    ))
-}
-
-async fn view_data(key: String, db: Arc<sled::Db>) -> Result<impl Reply, Rejection> {
-    if let Ok(Some(data)) = db.get(key.to_lowercase().as_str()) {
-        return Ok(warp::reply::with_status(
-            String::from_utf8_lossy(&data).to_string(),
-            http::StatusCode::FOUND,
-        ));
-    } else {
-        return Ok(warp::reply::with_status(
-            String::from("not found"),
-            http::StatusCode::NOT_FOUND,
-        ));
-    }
-}
-
+mod controller;
 #[tokio::main]
 async fn main() {
+    flexi_logger::Logger::with_env_or_str("info").format(flexi_logger::colored_default_format).start().unwrap();
     let db: Arc<sled::Db> = Arc::new(sled::open("db").unwrap());
     let config: Config = config::Config::load(None).await.unwrap_or_default();
     let db_filter = warp::any().map(move || db.clone());
@@ -141,11 +15,11 @@ async fn main() {
         .and(warp::multipart::form().max_length(config.max_length))
         .and(db_filter.clone())
         .and(warp::header::<String>("host"))
-        .and_then(upload);
+        .and_then(controller::upload);
     let view_route = warp::path!(String)
         .and(warp::get())
         .and(db_filter.clone())
-        .and_then(view_data);
+        .and_then(controller::view_data);
     let route = upload_route.or(view_route);
     warp::serve(route).run(([127, 0, 0, 1], config.port)).await;
 }
