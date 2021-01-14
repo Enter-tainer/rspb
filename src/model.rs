1fc1a948db3ddb32098468522d97b675e10d7871
mgt <mgt@oi-wiki.org>
2021-01-14 18:17:40 +0800

    feat(model): add CRD

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,10 @@
-use blake3::Hash;
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 use sled::transaction;
 use sled::IVec;
 use sled::Transactional;
-use std::ops::Deref;
-use transaction::{
-    ConflictableTransactionError, TransactionResult, TransactionalTree, UnabortableTransactionError,
-};
+use transaction::{ConflictableTransactionError, TransactionalTree};
+use uuid::Uuid;
 #[derive(Debug, Hash, PartialEq, Eq)]
 pub enum TreeNames {
     DataTree,
@@ -27,22 +24,23 @@ impl AsRef<[u8]> for TreeNames {
 #[derive(Debug, Clone)]
 pub struct DataTrees {
     pub db: sled::Tree,
-    pub short_to_long_db: sled::Tree,
-    pub custom_to_long_db: sled::Tree,
+    pub short_to_uuid_db: sled::Tree,
+    pub custom_to_uuid_db: sled::Tree,
 }
 
 impl DataTrees {
     pub fn new(database: sled::Db) -> Self {
         DataTrees {
             db: database.open_tree(TreeNames::DataTree).unwrap(),
-            short_to_long_db: database.open_tree(TreeNames::ShortNameTree).unwrap(),
-            custom_to_long_db: database.open_tree(TreeNames::CustomNameTree).unwrap(),
+            short_to_uuid_db: database.open_tree(TreeNames::ShortNameTree).unwrap(),
+            custom_to_uuid_db: database.open_tree(TreeNames::CustomNameTree).unwrap(),
         }
     }
 }
 
 pub enum DataBaseErrorType {
     Existed,
+    NotFound,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -61,28 +59,30 @@ impl TextItem {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct DateBaseItem {
+pub struct DataBaseItem {
     destroy_time: Option<DateTime<Utc>>,
     custom_url: Option<String>,
+    uuid: Uuid,
     hash: String,
     short: String,
     text: TextItem,
 }
 
-impl DateBaseItem {
+impl DataBaseItem {
     pub fn new(
         text: TextItem,
         custom_url: Option<String>,
         destroy_time: Option<DateTime<Utc>>,
-    ) -> DateBaseItem {
+    ) -> DataBaseItem {
         let hash = blake3::hash(text.get_data().as_bytes());
         let short = String::from(&hash.to_hex()[0..4]);
-        DateBaseItem {
+        DataBaseItem {
             destroy_time,
             custom_url,
             text,
             short,
             hash: String::from(hash.to_hex().as_str()),
+            uuid: Uuid::new_v4(),
         }
     }
 }
@@ -114,8 +114,8 @@ fn insert_when_not_exist_transaction<K: AsRef<[u8]> + Into<IVec>, V: Into<IVec>>
     return Err(ConflictableTransactionError::Conflict);
 }
 
-pub fn add_record(db: DataTrees, data: DateBaseItem) -> Result<(), DataBaseErrorType> {
-    let res = (&db.db, &db.short_to_long_db, &db.custom_to_long_db).transaction(
+pub fn add_record(db: DataTrees, data: DataBaseItem) -> Result<(), DataBaseErrorType> {
+    let res = (&db.db, &db.short_to_uuid_db, &db.custom_to_uuid_db).transaction(
         |(db, short_to_long_db, custom_to_long_db): &(
             TransactionalTree,
             TransactionalTree,
@@ -124,19 +124,19 @@ pub fn add_record(db: DataTrees, data: DateBaseItem) -> Result<(), DataBaseError
          -> Result<(), ConflictableTransactionError> {
             insert_when_not_exist_transaction(
                 db,
-                data.hash.as_bytes(),
+                data.uuid.as_bytes(),
                 bincode::serialize(&data).unwrap(),
             )?;
             insert_when_not_exist_transaction(
                 short_to_long_db,
                 data.short.as_bytes(),
-                data.hash.as_bytes(),
+                data.uuid.as_bytes(),
             )?;
             if let Some(special_url) = &data.custom_url {
                 insert_when_not_exist_transaction(
                     custom_to_long_db,
                     special_url.as_bytes(),
-                    data.hash.as_bytes(),
+                    data.uuid.as_bytes(),
                 )?;
             }
             Ok(())
@@ -148,4 +148,85 @@ pub fn add_record(db: DataTrees, data: DateBaseItem) -> Result<(), DataBaseError
     return Ok(());
 }
 
-// pub fn delete_record(db: sled::Db, key: String) -> Result<(), DataBaseErrorType> {}
+fn search_key_in_db(db: DataTrees, key: &[u8]) -> Result<TreeNames, DataBaseErrorType> {
+    // uuid -> short -> custom
+    if db.db.contains_key(key).unwrap_or(false) {
+        return Ok(TreeNames::DataTree);
+    }
+    if db.short_to_uuid_db.contains_key(key).unwrap_or(false) {
+        return Ok(TreeNames::ShortNameTree);
+    }
+    if db.custom_to_uuid_db.contains_key(key).unwrap_or(false) {
+        return Ok(TreeNames::CustomNameTree);
+    }
+    return Err(DataBaseErrorType::NotFound);
+}
+
+fn get_data_in_db(db: DataTrees, key: &[u8]) -> Result<DataBaseItem, DataBaseErrorType> {
+    let res = search_key_in_db(db.clone(), key)?;
+    let mut data: DataBaseItem;
+    match res {
+        TreeNames::DataTree => {
+            data = bincode::deserialize::<DataBaseItem>(&db.db.get(key).unwrap().unwrap()).unwrap();
+        }
+        TreeNames::ShortNameTree => {
+            let key = db.short_to_uuid_db.get(key).unwrap().unwrap();
+            data = bincode::deserialize::<DataBaseItem>(&db.db.get(key).unwrap().unwrap()).unwrap();
+        }
+        TreeNames::CustomNameTree => {
+            let key = db.custom_to_uuid_db.get(key).unwrap().unwrap();
+            data = bincode::deserialize::<DataBaseItem>(&db.db.get(key).unwrap().unwrap()).unwrap();
+        }
+    }
+    Ok(data)
+}
+
+pub fn delete_record(db: DataTrees, key: String) -> Result<(), DataBaseErrorType> {
+    let data = get_data_in_db(db.clone(), key.as_bytes())?;
+    let res = (&db.db, &db.short_to_uuid_db, &db.custom_to_uuid_db).transaction(
+        |(db, short_to_long_db, custom_to_long_db): &(
+            TransactionalTree,
+            TransactionalTree,
+            TransactionalTree,
+        )|
+         -> Result<(), ConflictableTransactionError> {
+            db.remove(key.as_bytes())?;
+            short_to_long_db.remove(data.short.as_bytes())?;
+            if let Some(url) = &data.custom_url {
+                custom_to_long_db.remove(url.as_bytes())?;
+            }
+            Ok(())
+        },
+    );
+    if res.is_err() {
+        return Err(DataBaseErrorType::NotFound);
+    }
+    Ok(())
+}
+
+fn query_record(db: DataTrees, key: String) -> Result<DataBaseItem, DataBaseErrorType> {
+    get_data_in_db(db, key.as_bytes())
+}
+
+// fn update_record(db: DataTrees, key: String, value: DataBaseItem) -> Result<(), DataBaseErrorType> {
+//     let data = get_data_in_db(db.clone(), key.as_bytes())?;
+//     let res = (&db.db, &db.short_to_uuid_db, &db.custom_to_uuid_db).transaction(
+//         |(db, short_to_long_db, custom_to_long_db): &(
+//             TransactionalTree,
+//             TransactionalTree,
+//             TransactionalTree,
+//         )|
+//          -> Result<(), ConflictableTransactionError> {
+//             db.insert(key.as_bytes(), bincode::serialize(&value).unwrap())?;
+//             short_to_long_db.insert(data.short.as_bytes(), value.short.as_bytes())?;
+//             if let Some(url) = &data.custom_url {
+//                 custom_to_long_db.insert(url.as_bytes(), va)?;
+//             }
+//             Ok(())
+//         },
+//     );
+//     if res.is_err() {
+//         return Err(DataBaseErrorType::NotFound);
+//     }
+//     Ok(())
+// }
